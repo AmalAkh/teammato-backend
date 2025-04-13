87b49a61e4b0b527e7e0dad4e6958689b4906a67
DenCoder.EXE <dencoder.exe@gmail.com>
2025-04-13 19:34:22 +0200

    minor comments

--- TeammatoBackend/Abstractions/SignInData.cs
+++ TeammatoBackend/Abstractions/SignInData.cs
@@ -2,14 +2,13 @@
 
 namespace TeammatoBackend.Abstractions
 {
-   
+    // This class represents the data required for a user to sign in (login) to the system.
     public class SignInData
     {
-        
+        // The user's login or username.
         public string Login{get;set;}
+
+        // The user's password, used to authenticate the user alongside the login.
         public string Password{get;set;}
-
-        
-
     }
 }
--- TeammatoBackend/Abstractions/User.cs
+++ TeammatoBackend/Abstractions/User.cs
@@ -7,29 +7,36 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TeammatoBackend.Abstractions
 {
-   
+    // This class represents a user within the application.
     public class User
     {
-        
+        // The user's nickname (username).
         public string NickName{get;set;}
         
-        
+        // The user's email address, used for authentication and notifications.
         public string Email{get;set;}
         
-
+        // The user's password, stored securely and used for authentication.
         public string Password{get;set;}
        
-        
+        // The user's profile image.
         public string ?Image{get;set;}
         
+        // The user's unique identifier.
         [Key]
         public string ?Id{get;set;}
-        
+
+        // A collection of languages that the user is associated with.
         public ICollection<Language> ?Languages {get;set;}
+
+        // A collection of messages that this user has sent.
         public ICollection<Message> ?Messages {get;set;}
+
+        // A collection of favorite games associated with this user.
         [JsonIgnore]
         public ICollection<FavoriteGame> ?FavoriteGames {get;set;}
 
+        // A collection of chats that this user is a participant in.
         [JsonIgnore]
         public ICollection<Chat> ?Chats {get;set;}
     }
